b6401115367bdb839a67cfb9b88d8aa452e8d640
Ragavendaran <ragavendaran.puli@gmail.com>
2020-05-28 21:23:28 +0530

    test subfolders

---
+++ 1. Addition of 2 numbers/add2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() {
+    float n1, n2;
+    printf("Number1: ");
+    scanf("%f", &n1);
+    printf("Number2: ");
+    scanf("%f", &n2);
+    printf("Sum: %f", n1+n2);
+}
